bccf39a2d06de7ce25c72bc9649a104a8e4b45c8
yrjanar <39477314+yrjanar@users.noreply.github.com>
2018-11-04 09:58:01 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -0,0 +1,17 @@
+package com.example.android.sunshine.sync;
+
+// TODO (9) Create a class called SunshineSyncUtils
+    //  TODO (10) Create a public static void method called startImmediateSync
+    //  TODO (11) Within that method, start the SunshineSyncIntentService
+
+import android.content.Context;
+import android.content.Intent;
+
+public class SunshineSyncUtils {
+
+    public static void startImmediateSync(final Context context) {
+        Intent intentToSyncImmediately = new Intent(context,
+                SunshineSyncIntentService.class);
+        context.startService(intentToSyncImmediately);
+    }
+}
